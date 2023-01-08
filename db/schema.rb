a59431fe7338881819ff18926c55919a77b5dab4
thianchai M1 <thianchai@ltman.com>
2023-01-08 22:33:22 +0700

    migrate admin to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_02_204700) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_08_152257) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_02_204700) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
